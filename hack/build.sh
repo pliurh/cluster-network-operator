d48bd3261d4618864ddc6e8a089498c429a4150c
Peng Liu <pliu@redhat.com>
2019-02-02 16:40:24 +0800

    Update host path

--- hack/build.sh
+++ hack/build.sh
@@ -2,5 +2,5 @@ rm  _output/linux/amd64/cluster-network-operator
 bash hack/build-go.sh
 env POD_NAME=LOCAL \
 	$(cat env.sh) \
-	OVN_IMAGE="quay.io/pliurh/ovn-daemonset:latest" \
+	OVN_IMAGE="quay.io/pliurh/ovn-daemonset:921fb1fc7b834a7b9e00d9b589f750cbead0b375" \
        	_output/linux/amd64/cluster-network-operator
